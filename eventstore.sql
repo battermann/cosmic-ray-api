ea8caa7b6bc9b376ec51de15d41f4104122fb838
Leif Battermann <leif.battermann@gmail.com>
2020-01-06 20:01:34 +0100

    publish events

--- eventstore.sql
+++ eventstore.sql
@@ -9,3 +9,10 @@ CREATE TABLE events(
     data json NOT NULL,
     PRIMARY KEY (stream_id, version)
 );
+
+CREATE FUNCTION publish_events()
+RETURNS void AS
+'   SELECT pg_notify(''events'', CONCAT(''['',version::varchar(255),'','',data #>> ''{}'','']'')) 
+    FROM events
+    ORDER BY time_stamp ASC;
+' LANGUAGE sql STABLE;
